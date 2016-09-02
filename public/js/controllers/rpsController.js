d0cfec96ac21e4bc2e485a901bb998987ce8e21d
Wardrt <ryanward1@btinternet.com>
2016-09-02 15:55:29 +0100

    Finished Rock Paper Scissors.

--- public/js/controllers/rpsController.js
+++ public/js/controllers/rpsController.js
@@ -3,19 +3,30 @@ angular
   .controller("rpsController", rpsController);
 
 function rpsController() {
-  this.choices   = [rock, paper, scissors];
-  this.userScore = 0;
-  this.botScore  = 0;
+  this.choices      = [rock, paper, scissors];
+  this.userScore    = 0;
+  this.botScore     = 0;
+  this.roundsPlayed = 0;
   var userChoice;
   var botChoice;
 
   this.play = function(userPlay) {
-    userChoice = userPlay;
-    this.botsPlay();
-    this.evaluate();
-    this.updateScoreboard();
-    this.updateStatus();
-  }
+    this.roundsPlayed++;
+    if (this.roundsPlayed < 3) {
+      userChoice = userPlay;
+      this.botsPlay();
+      this.evaluate();
+      this.updateScoreboard();
+      this.updateStatus();
+    } else if (this.roundsPlayed === 3) {
+      userChoice = userPlay;
+      this.botsPlay();
+      this.evaluate();
+      this.updateScoreboard();
+      this.updateStatus();
+      this.winner();
+    }
+}
 
   this.botsPlay = function() {
     var index = Math.floor(Math.random() * this.choices.length);
@@ -51,13 +62,24 @@ function rpsController() {
   }
 
   this.restart = function() {
-    this.userScore = 0;
-    this.botScore  = 0;
+    this.userScore    = 0;
+    this.botScore     = 0;
+    this.roundsPlayed = 0;
     this.updateScoreboard();
     this.removeStatus();
   }
 
   this.removeStatus = function() {
     document.getElementById('status').innerText = "";
+  }
+
+  this.winner = function() {
+    if (this.userScore > this.botScore) {
+      document.getElementById('result').innerText = "You Win! Hit Restart to play again.";
+    } else if (this.botScore > this.userScore) {
+      document.getElementById('result').innerText = "You Lose, hit restart to try to beat the bot again";
+    } else {
+      document.getElementById('result').innerText = "It's a tie!"
+    }
   }
 }
